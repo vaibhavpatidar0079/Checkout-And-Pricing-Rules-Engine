b245da89290bc77afdae71ec228b536b18554580
Vaibhav <vaibhavpatidar0079@gmail.com>
2026-07-13 19:46:48 +0530

    add Separation of content by line for user menu

--- src/main/java/org/vaibhav/Menu.java
+++ src/main/java/org/vaibhav/Menu.java
@@ -11,35 +11,35 @@ public class Menu {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("customer type:");
-
         System.out.println("1. Regular");
         System.out.println("2. Silver");
         System.out.println("3. Gole");
         System.out.print("Enter no. ");
 
-        int type = sc.nextInt();
-        if(type>3 || type<1){
-            System.out.println("Type can only be 1, 2 and 3");
-            return takeTypeInput();
-        }
-        return type;
+        return sc.nextInt();
     }
 
     static User makeUser(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
+        int typeInput = takeTypeInput();
 
-        int type = takeTypeInput() - 1; //input start from 1 but in DataBase it start from 0
+        while(typeInput < 1 || typeInput > 3 ) { //input start from 1 but in DataBase it start from 0
+            System.out.println("-".repeat(60));
+            System.out.println("Type can only be 1, 2 and 3");
+            System.out.println("-".repeat(60));
+            typeInput = takeTypeInput();
+        }
 
-        User u = new User(name, type);
-
+        User u = new User(name, typeInput);
+        System.out.println("-".repeat(60));
         System.out.println("account created");
         System.out.println("Name: "+ u.name);
         System.out.println("Account type: "+ u.getType());
         System.out.println("-".repeat(60));
 
-        return new User(name, type);
+        return new User(name, typeInput);
 
     }
     public static String[] TakeCartInput(){
