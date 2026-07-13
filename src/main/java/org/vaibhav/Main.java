e384a03411bc8e4695948c9802fd1538f4d2b2c2
Vaibhav <vaibhavpatidar0079@gmail.com>
2026-07-13 14:21:40 +0530

    Initial commit

---
+++ src/main/java/org/vaibhav/Main.java
@@ -0,0 +1,10 @@
+package org.vaibhav;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main() {
+
+    }
+    }
+}
---
+++ src/main/java/org/vaibhav/User.java
@@ -0,0 +1,7 @@
+package org.vaibhav;
+
+public class User {
+    String name;
+    int type; //0 = regular, 1 = silver, 3 = gold
+
+}
