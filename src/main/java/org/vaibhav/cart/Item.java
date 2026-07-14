e6c6cd0be0badff8beb611e53e1692641c0e3613
Vaibhav <vaibhavpatidar0079@gmail.com>
2026-07-14 22:11:06 +0530

    fix Item argument type

--- src/main/java/org/vaibhav/cart/Item.java
+++ src/main/java/org/vaibhav/cart/Item.java
@@ -4,7 +4,7 @@ public class Item {
     String name;
     double price;
 
-    public Item(String name, int price){
+    public Item(String name, double price){
         this.name = name;
         if(price > 0) this.price = price;
         else throw new IllegalArgumentException();
